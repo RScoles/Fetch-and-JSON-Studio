25e1a0534eb76a52ab44088f0e840c34f8760b30
RScoles <ryanscoles@ryans-Air.attlocal.net>
2023-03-08 14:56:27 -0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // TODO: add code here
+window.addEventListener ('load', function() {
+
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then(function(response) {
+        return response.json();
+    }).then(function(response) {
+
+        let sortedAstronaunts = response.sort(function (a, b) {return a.hoursInSpace - b.hoursInSpace });
+        let myContainer = document.getElementById('container');
+
+       let allMyHtml = "";
+
+       let myCount = `
+       <h3>Astronaunts Count: ${sortedAstronaunts.length}</h3>
+       `
+       allMyHtml += myCount;
+       sortedAstronaunts.forEach(function(astronaunt) {
+            let myHtml = `
+<div class="astronaut">
+    <div class="bio">
+        <h3>${astronaunt.firstName} ${astronaunt.lastName}</h3>
+        <ul>
+            <li>Hours in Space:${astronaunt.hoursInSpace}</li>
+            <li ${astronaunt.active ? 'style="color:green;"' : ''}>Active: ${astronaunt.active}</li>
+            <li>Skills: ${astronaunt.skills.join(", ")}</li>
+        </ul>
+    </div>
+    <img class="avatar" src="${astronaunt.picture}">
+</div>
+            `;
+           allMyHtml += myHtml;
+        
+        });
+
+    myContainer.innerHTML = allMyHtml;
+    });
+
+});
